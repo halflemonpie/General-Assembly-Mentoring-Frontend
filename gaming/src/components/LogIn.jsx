13ffd81bffca3e687c61025bbb48765a74e5b018
Leon Tzu Chiang Chan <leontzuchiangchan@Leons-MacBook-Air.local>
2022-09-15 17:04:48 -0400

    Leon part

---
+++ gaming/src/components/LogIn.jsx
@@ -0,0 +1,23 @@
+// export default function LogIn() {}
+
+export default function LogIn({ handleEmailChange, handlePasswordChange, handleLogIn }) {
+ 
+  return (
+    <div>
+        <h1>Log In</h1>
+      <form>
+        <div>
+          <label htmlFor='email'>Email </label>
+          <input type='text' name="email" onChange={handleEmailChange}/>
+        </div>
+        <div>
+          <label htmlFor='password'>Password </label>
+          <input type='password' name="email" onChange={handlePasswordChange}/>
+        </div>
+        <div>
+          <button className="sign-in-btn" type='submit' value="Submit" onClick={handleSignIn}>Log In</button>
+        </div>
+      </form>
+    </div>
+  )
+}
---
+++ gaming/src/components/MainBoard.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export default function MainBoard({ MainBoard }) {
+  
+  return (
+    <div>
+      <div className='MainBoard-component'></div>
+    </div>
+  )
+}
